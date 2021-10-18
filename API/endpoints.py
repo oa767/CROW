500b6004f7fafc53b7e3b711629ac07125bd5783
Omar AbouelNour <oa767@nyu.edu>
2021-10-18 13:23:51 -0400

    Trying out travis

--- API/endpoints.py
+++ API/endpoints.py
@@ -5,7 +5,7 @@ The endpoint called `endpoints` will return all available endpoints.
 
 from flask import Flask
 from flask_restx import Resource, Api
-import db.db as db
+# import db.db as db
 
 app = Flask(__name__)
 api = Api(app)
