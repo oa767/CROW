a3d6a703cc1ae4f217b472b116115ba8e5af16fd
oa767 <47250954+oa767@users.noreply.github.com>
2021-11-09 13:07:07 -0500

    Broke down makefile into multiple makefiles; Added Test Mode for DB testing

--- db/db.py
+++ db/db.py
@@ -9,8 +9,14 @@ import os
 import random
 
 APP_HOME = os.environ["APP_HOME"]
+TEST_MODE = os.environ.get("TEST_MODE", 0)
+DB_DIR = f"{APP_HOME}/db"
 
 ROOMS_DB = f"{APP_HOME}/db/rooms.json"
+if TEST_MODE:
+    ROOMS_DB = f"{DB_DIR}/test_rooms.json"
+else:
+    ROOMS_DB = f"{DB_DIR}/rooms.json"
 
 OK = 0
 NOT_FOUND = 1
@@ -18,6 +24,9 @@ DUPLICATE = 2
 
 
 def write_rooms(rooms):
+    """
+    Write out the in-memory room list in proper DB format.
+    """
     with open(ROOMS_DB, 'w') as f:
         json.dump(rooms, f, indent=4)
 
