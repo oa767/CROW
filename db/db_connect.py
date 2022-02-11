7c54070b97892d69f75ad2e283706712bd871893
oa767 <47250954+oa767@users.noreply.github.com>
2022-02-10 23:15:43 -0500

    Trying - Trial #2

--- db/db_connect.py
+++ db/db_connect.py
@@ -57,7 +57,7 @@ def del_one(collect_nm, filters={}):
 def fetch_all(collect_nm, key_nm):
     all_docs = []
     for doc in client[db_nm][collect_nm].find():
-        # print(doc)
+        print(doc)
         all_docs[doc[key_nm]] = json.loads(bsutil.dumps(doc))
         all_docs.append(json.loads(bsutil.dumps(doc)))
     return all_docs
