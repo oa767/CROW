291d32d3b22b9275425e04ba3af483f05a392480
oa767 <47250954+oa767@users.noreply.github.com>
2022-04-28 18:33:37 -0400

    Defined Scheduler Jobs - Testing

--- db/scheduler.py
+++ db/scheduler.py
@@ -1,9 +1,30 @@
 from schedule import every, repeat, run_pending
 import time
+import db.data as db
+import db.db_connect as db_connect
+
+@repeat(every(1).minute)
+def test_scheduler():
+    db.add_room("test_room")
+
+@repeat(every(1).minute)
+def test2_scheduler():
+    try:
+        db.delete_room("test_room")
+    except:
+        pass
 
 @repeat(every(24).hours)
-def job():
-    print("Testing")
+def auto_delete_rooms():
+    lst = db.get_rooms()
+    for room in lst:
+        db.delete_room(room)
+
+@repeat(every(24).hours)
+def auto_delete_users():
+    lst = db.get_users()
+    for user in lst:
+        db.delete_user(user)
 
 while True:
     run_pending()
